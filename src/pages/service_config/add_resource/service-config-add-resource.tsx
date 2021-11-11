f8d09f2d695b7ea010d5599a8c21d2c52e9197c9
IFLYTEK\jiesun6 <jiesun6@iflytek.com>
2021-11-11 17:53:34 +0800

    feat: 应用详情添加应用 中上传 详情图片改为非必填

--- src/pages/service_config/add_resource/service-config-add-resource.tsx
+++ src/pages/service_config/add_resource/service-config-add-resource.tsx
@@ -473,10 +473,10 @@ export default () => {
               <Radio.Group
                 onChange={(e) => {
                   setIsSkip(e.target.value);
-                  if (e.target.value === 1) {
-                    // 清空 上传详情
-                    form.setFieldsValue({ detailPdfId: undefined });
-                  } else form.setFieldsValue({ url: undefined });
+                  // if (e.target.value === 1) {
+                  //   // 清空 上传详情
+                  //   // form.setFieldsValue({ detailPdfId: undefined });
+                  // } else form.setFieldsValue({ url: undefined });
                 }}
               >
                 <Radio value={1}>是</Radio>
@@ -500,12 +500,16 @@ export default () => {
               <Form.Item
                 name="detailPdfId"
                 label="上传详情"
-                rules={[
-                  {
-                    required: true,
-                    message: '必填',
-                  },
-                ]}
+                rules={
+                  isEditing
+                    ? undefined
+                    : [
+                        {
+                          required: true,
+                          message: '必填',
+                        },
+                      ]
+                }
               >
                 <UploadForm
                   listType="picture-card"
