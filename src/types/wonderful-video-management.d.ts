2325f40e39e4a61ff57b3023b0409e35274ba3dc
jingqin5 <jingqin5@iflytek.com>
2022-06-22 20:55:10 +0800

    footer

---
+++ src/types/wonderful-video-management.d.ts
@@ -0,0 +1,22 @@
+import type Common from '../common';
+
+namespace WonderfulVideoManagement {
+  export type RecordList = {
+    result: Content[];
+  } & Common.ResultCode &
+    Common.ResultPage;
+
+  export type Content = {
+    id?: integer; // userid
+    userName?: string; // 用户名
+    viewRange?: string; // 查看范围
+    viewRangeIds?: string[]; // 查看范围id
+    creator?: string; // 创建人
+    createTime?: string; // 创建时间
+    isEdit?: boolean; // 能否编辑
+    name?: string;
+    typeIds?: any;
+    lineStatus?: boolean;
+  };
+}
+export default WonderfulVideoManagement;
