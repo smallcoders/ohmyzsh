1c9df48dd17c06357db5111aef3ad9a3238f53fd
lijiejun <703491736@qq.com>
2022-09-20 09:10:49 +0800

    修复面包屑多余name；修复字段信息拼接

--- config/routes.ts
+++ config/routes.ts
@@ -984,7 +984,6 @@ export default [
           },
           {
             path: routeName.BANKING_SERVICE_INDEX,
-            name: '金融服务管理',
             component: './service_config/banking_service_manage/index',
           },
           {
