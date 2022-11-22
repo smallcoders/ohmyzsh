3772eaea7849e7277f97e0c7f03024bd17d41ac1
chaotang3 <chaotang3@iflytek.com>
2022-11-22 21:27:47 +0800

    add-运营活动管理优化

--- src/pages/operation_activity/link-setting/index.tsx
+++ src/pages/operation_activity/link-setting/index.tsx
@@ -937,6 +937,7 @@ export default () => {
         width="600px"
         maskClosable={false}
         visible={createModalVisible}
+        className={sc('formModel')}
         onCancel={() => {
           clearForm();
           setCurrent(0)
