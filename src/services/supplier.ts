731ca492a32b816508050cead46af817e5805a5a
qqqian <qqqian@iflytek.com>
2022-11-23 14:44:58 +0800

    编辑修改

--- src/services/supplier.ts
+++ src/services/supplier.ts
@@ -22,7 +22,7 @@ export async function removeSupplier(id: number) {
 
 // 从供应链查询供应商信息
 export async function queryGyl(data?: Record<string, any>) {
-  return request<Common.ResultCode>('/antelope-finance/mng/supplier/queryGyl', {
+  return request<any>('/antelope-finance/mng/supplier/queryGyl', {
     method: 'post',
     data,
   });
@@ -30,7 +30,7 @@ export async function queryGyl(data?: Record<string, any>) {
 
 // 天眼查
 export async function queryOrg(data?: Record<string, any>) {
-  return request<Common.ResultCode>('/antelope-other/org/queryOrg', {
+  return request<any>('/antelope-other/org/queryOrg', {
     method: 'post',
     data,
   });
@@ -49,14 +49,14 @@ export async function saveOrUpdateSupplier(data?: Record<string, any>) {
 
 // 查询供应商详情
 export async function detailSupplier(id: number) {
-  return request<Common.ResultCode>(`/antelope-finance/mng/supplier/getSupplierDetail?id=${id}`, {
+  return request<any>(`/antelope-finance/mng/supplier/getSupplierDetail?id=${id}`, {
     method: 'get',
   });
 }
 
 // 获取模板文件
 export async function getTemplateFile(fileType: string = 'gyl') {
-  return request<Common.ResultCode>(
+  return request<any>(
     `/antelope-finance/myLoan/getTemplateFile?fileType=${fileType}`,
     {
       method: 'get',
