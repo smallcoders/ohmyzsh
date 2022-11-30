cabeda00be1a9f990fc6b2389e746c1f659bc380
yyjiang6 <https://yyjiang6@git.iflytek.com>
2022-11-30 16:45:03 +0800

    文件上传组件删除value值

--- src/components/upload_form/upload-form.tsx
+++ src/components/upload_form/upload-form.tsx
@@ -101,12 +101,13 @@ const UploadForm = (
     }
     return true;
   };
-
+const p = {...props}
+delete p.value
   return (
     <>
       {props.tooltip}
       <Upload
-        {...props}
+        {...p}
         name="file"
         action={props.action || '/antelope-manage/common/upload'}
         onChange={handleChange}
