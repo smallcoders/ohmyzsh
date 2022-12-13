83d1600b4f02b96ccf5be9f5dd074b312f2850c4
chaotang3 <chaotang3@iflytek.com>
2022-12-13 16:13:53 +0800

    update-用户信息订正

--- src/pages/operation_activity/link_setting/index.tsx
+++ src/pages/operation_activity/link_setting/index.tsx
@@ -330,7 +330,7 @@ export default () => {
             value.url=res?.result
           }
         }
-        value.activeUrl= window.location.protocol + '//' + window.location.host.split(':')[0] +`/antelope-activity-h5/share-code/index.html?preview=true&targetLinkType=${value.targetLinkType}&buttonText=${value.buttonText}&targetLink=${value.targetLink}&url=${value.url}`
+        value.activeUrl= `https://www.lingyangplat.com/antelope-activity-h5/share-code/index.html?preview=true&targetLinkType=${value.targetLinkType}&buttonText=${value.buttonText}&targetLink=${value.targetLink}&url=${value.url}`
         setCurrent(1)
         setFormData(value)
       }else if(edge==3){
@@ -379,12 +379,12 @@ export default () => {
     console.log(e)
     if(e.id){
       // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-      copy(window.location.protocol + '//' + window.location.host.split(':')[0] +`/antelope-activity-h5/share-code/index.html?preview=false&targetLinkType=${e.targetLinkType}&id=${e.id}`)
+      copy(`https://www.lingyangplat.com/antelope-activity-h5/share-code/index.html?preview=false&targetLinkType=${e.targetLinkType}&id=${e.id}`)
         message.success('链接复制成功');
 
     }else{
       // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-      copy(window.location.protocol + '//' + window.location.host.split(':')[0] +`/antelope-activity-h5/share-code/index.html?preview=true&targetLinkType=${e.targetLinkType}&buttonText=${e.buttonText}&targetLink=${e.targetLink}&url=${e.url}`)
+      copy(`https://www.lingyangplat.com/antelope-activity-h5/share-code/index.html?preview=true&targetLinkType=${e.targetLinkType}&buttonText=${e.buttonText}&targetLink=${e.targetLink}&url=${e.url}`)
         message.success('链接复制成功');
     }
   };
@@ -1125,7 +1125,7 @@ export default () => {
             <h2 >以下链接用于预览效果用，不计入数据统计</h2>
             {formData&&(
               <div>
-            <h2 className={sc('modelWord-link')}>{ window.location.protocol + '//' + window.location.host.split(':')[0] +`/antelope-activity-h5/share-code/index.html?preview=true&targetLinkType=${formData.targetLinkType}&buttonText=${formData.buttonText}&targetLink=${formData.targetLink}&url=${formData.url}`}
+            <h2 className={sc('modelWord-link')}>{ `https://www.lingyangplat.com/antelope-activity-h5/share-code/index.html?preview=true&targetLinkType=${formData.targetLinkType}&buttonText=${formData.buttonText}&targetLink=${formData.targetLink}&url=${formData.url}`}
             </h2>
             <Button
               type="primary"
@@ -1148,7 +1148,7 @@ export default () => {
               <div className={sc('modelWord-bk-invite')}>邀请人：{shardCodeMaster}</div>
               <div className="qr-anhui-pf">
                 <QRCode
-                  value={(window.location.protocol + '//' + window.location.host.split(':')[0] +`/antelope-activity-h5/antelope-download/index.html?shardCodeMaster=${shardCodeMaster}&preview=true`) }
+                  value={(`https://www.lingyangplat.com/antelope-activity-h5/antelope-download/index.html?shardCodeMaster=${shardCodeMaster}&preview=true`) }
                   renderAs={'canvas'}
                   size={113}
                   bgColor={'#FFFFFF'}
@@ -1162,7 +1162,7 @@ export default () => {
               <div className={sc('modelWord-bk1-invite')}>邀请人：{shardCodeMaster}</div>
               <div className="qr-anhui-pf">
                 <QRCode
-                  value={(window.location.protocol + '//' + window.location.host.split(':')[0] +`/antelope-activity-h5/antelope-download/index.html?shardCodeMaster=${shardCodeMaster}&preview=false`)}
+                  value={(`https://www.lingyangplat.com/antelope-activity-h5/antelope-download/index.html?shardCodeMaster=${shardCodeMaster}&preview=false`)}
                   renderAs={'canvas'}
                   size={113}
                   bgColor={'#FFFFFF'}
