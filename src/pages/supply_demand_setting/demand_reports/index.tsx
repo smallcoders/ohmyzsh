7c5d60c636fb6c56651fcdb6245f3c3c580242e4
junqin5 <junqin5@iflytek.com>
2022-12-21 13:25:23 +0800

    feat✨:  数据更新时间显示和数据传递

--- src/pages/supply_demand_setting/demand_reports/index.tsx
+++ src/pages/supply_demand_setting/demand_reports/index.tsx
@@ -1,9 +1,10 @@
 import TabMenu from '@/components/TabMenu';
-import { useState, useEffect } from 'react';
+import { useState, useEffect} from 'react';
 import { history } from 'umi';
 
 export default () => {
-  const [activeKey, setActiveKey] = useState<string>('1');
+  const [activeKey, setActiveKey] = useState<string>('');
+
   const { type } = history.location.query as any;
 
   const prepare = async () => {
@@ -16,18 +17,28 @@ export default () => {
     prepare();
   }, []);
 
+  useEffect(() => {
+    console.log('activeKey =>', activeKey)
+  }, [activeKey])
+
   return (
     <div>
-      <TabMenu tabs={['M_SD_BBZB', 'M_SD_BBMXB', 'M_SD_BBZBB', 'M_SD_BBYBB']} activeState={activeKey}></TabMenu>
-      <div style={
-        {
-          position: 'absolute',
-          top: 85,
-          right: 5
-        }
-      }>
-        数据更新时间：2022-09-12 23:00:00
-      </div>
+      <TabMenu
+        tabs={['M_SD_BBZB', 'M_SD_BBMXB', 'M_SD_BBZBB', 'M_SD_BBYBB']}
+        activeState={'1'}
+        setActiveKey={setActiveKey}
+      />
+      { activeKey !== 'M_SD_BBMXB' ?
+        <div style={
+            {
+              position: 'absolute',
+              top: 85,
+              right: 5
+            }
+          }>
+          数据更新时间：2022-09-12 23:00:00
+        </div> : null
+      }
     </div>
   );
 };
