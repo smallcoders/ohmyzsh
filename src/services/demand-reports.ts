c6672c96833a3b6bff3f7701922b5766dd0dce71
junqin5 <junqin5@iflytek.com>
2022-12-21 15:22:44 +0800

    feat✨:  总表基本开发完成

---
+++ src/services/demand-reports.ts
@@ -0,0 +1,25 @@
+import type Common from '@/types/common';
+// import type OrgTypeManage from '@/types/org-type-manage';
+import { request } from 'umi';
+
+/**
+ * 供需对接报表 - 总表
+ */
+export async function getDemandReportsTotalList(data?: Record<string, any>) {
+  return request<Common.ResultCode & { result: any }>(
+    '/antelope-manage/needTable/totalNumber', {
+      method: 'post',
+      data,
+    }
+  );
+}
+
+// 导出-服务管理-意向消息导出
+export async function exportTotalTable() {
+  return request<any>('/antelope-manage/intendMessage/export',{
+    method: 'post',
+    data: {},
+    responseType: 'blob',
+    getResponse: true,
+  })
+}
