5e76c01b48366acd96d585c9c4690cc7d2440914
chaotang3 <chaotang3@iflytek.com>
2022-12-26 10:14:51 +0800

    add-科产专栏

--- src/pages/science_technology_manage/column_manage/edit/index.tsx
+++ src/pages/science_technology_manage/column_manage/edit/index.tsx
@@ -3,73 +3,114 @@ import {PageContainer} from "@ant-design/pro-layout";
 import './index.less';
 import scopedClasses from "@/utils/scopedClasses";
 import ProCard from "@ant-design/pro-card";
-import {Button, Form, Input} from "antd";
+import {Button, Form, Input, message} from "antd";
 import {history} from "@@/core/history";
 import UploadForm from "@/components/upload_form";
+import {getColumnDetailById, saveColumn} from "@/services/column-manage";
+import {useEffect} from "react";
 const sc = scopedClasses('column-manage-edit');
 export default () => {
+  const creativeColumnId = history.location.query?.id as string;
+
   const [form] = Form.useForm();
+  const getColumnDetail = async () =>{
+    try {
+      const res = await getColumnDetailById(creativeColumnId)
+      if(res.code==0){
+        console.log(res)
+        form.setFieldsValue(res.result)
+      }
+    }catch (e){
+      console.log(e)
+    }
+  }
+  useEffect(() => {
+    getColumnDetail()
+  }, [creativeColumnId]);
+  //新增和修改的发布按钮
+  const onFinish = async () => {
+    form
+      .validateFields()
+      .then(async (value: any) => {
+        try {
+          const res = await saveColumn({...value,id:creativeColumnId})
+         if(res.code==0){
+           message.success(`发布成功`);
+           history.goBack()
+         }
+        }catch (e){
+          console.log(e)
+        }
+      })
+      .catch((err: any)=>{
+        console.log(err)
+      })
+
+  };
   return(
     <PageContainer className={sc('container')}>
       <Form className={sc('container-form')} form={form}>
-        <Form.Item name="name" label="专栏名称" required>
-          <Input  />
+        <Form.Item name="name" label="专栏名称" required   rules={[{ required: true, message: '请输入专栏名称！' }]}>
+          <Input placeholder={'请输入'} />
         </Form.Item>
         <Form.Item
-          name="logoImageId"
+          name="content"
           label="专栏介绍"
           required
+          rules={[{ required: true, message: '请输入专栏介绍！' }]}
         >
-          <Input.TextArea showCount maxLength={300}  />
+          <Input.TextArea showCount maxLength={300}  placeholder={'请输入'}/>
         </Form.Item>
         <Form.Item
-          name="logoImageId"
-          label="上传图片"
-          required
+          label="服务端"
           className={'pics'}
         >
-·          <Form.Item name="actualCapital" label="Web端图片" required className={'pic'}>
+          <Form.Item  label="Web端" required className={'pic'}>
+            <Form.Item style={{backgroundColor:"#F8F9FA"}}  name="webPhotoId" label="Web端banner图片" required className={'link'} rules={[{ required: true, message: '请输入Web端banner图片！' }]}>
             <UploadForm
               listType="picture-card"
               className="avatar-uploader"
               showUploadList={false}
               maxSize={5}
-              accept=".bmp,.gif,.png,.jpeg,.jpg"
+              accept=".png,.jpg"
             />
+            </Form.Item>
+            <div className={'pic-word'} style={{marginBottom:'20px'}}>限定一张图片，不超过5M，<span className={'red'}>尺寸（以实际效果图为准）</span>，格式支持png、jpg;</div>
+            <Form.Item style={{backgroundColor:"#F8F9FA"}}  name="webLink" label="Web端链接地址" required className={'link'} rules={[{ required: true, message: '请输入Web端链接地址！' }]}>
+            <Input  placeholder={'请输入Web端链接'}/>
+            </Form.Item>
           </Form.Item>
-          <div style={{marginBottom:'20px'}}>限定一张图片，不超过5M，尺寸（以实际效果图为准），格式支持png、jpg</div>
-          <Form.Item name="actualCapital" label="App端图片" className={'pic'}>
-            <UploadForm
-              listType="picture-card"
-              className="avatar-uploader"
-              showUploadList={false}
-              maxSize={5}
-              accept=".bmp,.gif,.png,.jpeg,.jpg"
-            />
+          <Form.Item  label="App端"  className={'pic'}>
+            <Form.Item style={{backgroundColor:"#F8F9FA"}} name="appPhotoId" label="App端banner图片"  className={'link'} >
+              {/*<div className={'pic-content'}>*/}
+              <UploadForm
+                listType="picture-card"
+                className="avatar-uploader"
+                showUploadList={false}
+                maxSize={5}
+                accept=".png,.jpg"
+              />
+              {/*</div>*/}
+            </Form.Item>
+            <div className={'pic-word'} style={{marginBottom:'20px'}}>限定一张图片，不超过5M，<span className={'red'}>尺寸（以实际效果图为准）</span>，格式支持png、jpg;</div>
+            <Form.Item style={{backgroundColor:"#F8F9FA"}} name="appLink" label="App端链接地址" className={'link'} >
+              <Input  placeholder={'请输入App端链接'}/>
+            </Form.Item>
           </Form.Item>
-          <div style={{marginBottom:'20px'}}>限定一张图片，不超过5M，尺寸（以实际效果图为准），格式支持png、jpg</div>
         </Form.Item>
-        <Form.Item  name="regCapital" label="链接地址" required>
-          <Form.Item name="actualCapital" label="Web端" required className={'link'}>
-            <Input  />
-          </Form.Item>
-          <Form.Item name="actualCapital" label="App端" className={'link'}>
-            <Input  />
-          </Form.Item>
-        </Form.Item>
-        <Form.Item name="actualCapital" label="展示顺序" >
-          <Input  />
+        <Form.Item name="sort" label="展示顺序" >
+          <Input   placeholder={'请输入'}/>
         </Form.Item>
         <Form.Item
-          name="logoImageId"
+          name="remark"
           label="备注"
         >
-          <Input.TextArea showCount maxLength={300}  />
+          <Input.TextArea showCount maxLength={300} placeholder={'请输入'} />
         </Form.Item>
       </Form>
       <ProCard layout="center">
         <Button style={{marginRight:'40px'}} onClick={() => history.goBack()}>返回</Button>
-        <Button type={"primary"} onClick={() => history.goBack()}>发布</Button>
+        <Button type={"primary"} onClick={onFinish}>发布</Button>
       </ProCard>
     </PageContainer>
   )
--- src/pages/science_technology_manage/column_manage/index.tsx
+++ src/pages/science_technology_manage/column_manage/index.tsx
@@ -1,34 +1,256 @@
 import {PageContainer} from "@ant-design/pro-layout";
-import React, {useState} from "react";
-import {Button, Col, Form, Input, message, Popconfirm, Row, Select, Space, Switch, Table} from "antd";
-import moment from "moment/moment";
+import React, {useEffect, useState} from "react";
+import {
+  Button,
+  Col,
+  Form,
+  Input,
+  Tooltip,
+  Drawer,
+  Popconfirm,
+  Row,
+  Select,
+  Space,
+  Switch,
+  Table, message,Dropdown,
+  Menu,
+} from "antd";
 import './index.less';
 import scopedClasses from "@/utils/scopedClasses";
-import Activity from "@/types/operation-activity";
 import {history} from "@@/core/history";
-import {routeName} from "../../../../config/routes";
+import { DownOutlined ,UploadOutlined} from '@ant-design/icons';
+import {
+  columnOffShelf,
+  columnOnShelf,
+  getClickPageDetailById,
+  getColumnByPage, loanClickExport,
+  removeColumn
+} from "@/services/column-manage";
 const sc = scopedClasses('column-manage');
 export default () => {
-  // const [dataSource, setDataSource] = useState<any>([]);
-  const dataSource=[
-    {name:'nihao',intro:'rewq1',started:true,num:2341},
-    {name:'nihao1',intro:'rewq2',started:false,num:2341},
-    {name:'nihao2',intro:'rewq3',started:true,num:2341},
-    {name:'nihao3',intro:'rewq4',started:false,num:2341},
-  ]
+  const [dataSource, setDataSource] = useState<any>([]);
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const [clickDataSource, setClickDataSource] = useState<any>([]);
   const [searchContent, setSearChContent] = useState<{
-    activeName?: string; // 活动名称
-    startTime?: string; // 活动时间
-    endTime?: string; // 活动时间
-    activeChannelId?: number; // 状态：0发布中、1待发布、2已下架
-    activeSceneId?: number; // 状态：0发布中、1待发布、2已下架
+    name?: string; // 名称
+    shelfStatus?: boolean; // 状态：true上架 false下架
   }>({});
   const [pageInfo, setPageInfo] = useState({
     pageIndex: 1,
     pageSize: 10,
-    total: 1,
+    totalCount: 0,
   });
+  const [clickPageInfo, setClickPageInfo] = useState({
+    pageIndex: 1,
+    pageSize: 10,
+    totalCount: 1,
+  });
+  const [clickOpen, setClickOpen] = useState(false);
+  const showDrawer = async (e: any,_record: any) => {
+    setClickOpen(true);
+    try{
+      const data = {creativeColumnId:_record.id,pageSize:10,pageIndex:1}
+      const res =await getClickPageDetailById(data)
+      if(res.code==0){
+        console.log(res)
+      }
+    }catch (err: any){
+      console.log(err)
+    }
+  };
+  const onClickClose = () => {
+    setClickOpen(false);
+  };
 
+  //方法
+  /*
+  * 获取分页数据
+  * @param pageIndex
+  * @param pageSize
+  * @param activeType
+  */
+  const getColumnList = async (pageIndex: number = 1, pageSize = pageInfo.pageSize) => {
+    try {
+      const { result,totalCount, code } = await getColumnByPage({
+        pageIndex,
+        pageSize,
+        ...searchContent,
+      })
+      if (code === 0) {
+        setDataSource(result)
+        setPageInfo({ totalCount, pageIndex, pageSize });
+      } else {
+        message.error(`请求分页数据失败`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getColumnList();
+  }, [searchContent]);
+
+  //上下架
+  const changeShelfStatus = async (shelfStatus: boolean,record: any) =>{
+    console.log(shelfStatus,record)
+    if(!shelfStatus){
+      try {
+        const res = await columnOnShelf({id:record.id})
+        if(res.code==0){
+          await  getColumnList()
+          console.log(res)
+        }
+      }catch (e){
+
+      }
+    }else{
+      try {
+        const res = await columnOffShelf({id:record.id})
+        if(res.code==0){
+          await  getColumnList()
+          console.log(res)
+        }
+      }catch (e){
+
+      }
+    }
+  }
+
+  //删除按钮
+  const remove = async (record: any) =>{
+    try {
+      const res = await removeColumn(record.id)
+      if (res.code === 0) {
+        message.success(`删除成功`);
+        await  getColumnList();
+      } else {
+        message.error(`删除失败，原因:{${res.message}}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const columns = [
+    {
+      title: '顺序序号',
+      dataIndex: 'sort',
+      key: 'sort',
+      width:100,
+      render: (_: any, _record: any, index: number) =>
+        pageInfo.pageSize * (pageInfo.pageIndex - 1) + index + 1,
+    },
+    {
+      title: '专栏名称',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '专栏介绍',
+      dataIndex: 'content',
+      key: 'content',
+      ellipsis: true,
+    },
+    {
+      title: '上下架状态',
+      key: 'shelfStatus',
+      dataIndex: 'shelfStatus',
+      width:120,
+      render: (shelfStatus: boolean,_record: any) => {
+        return (
+          <Popconfirm
+            title={
+              (shelfStatus&&
+                <div className="className">
+                  <div>提示</div>
+                  <div>是否下架</div>
+                </div>)||  (!shelfStatus&&
+                <div className="className">
+                  <div>提示</div>
+                  <div>是否上架</div>
+                </div>)
+            }
+            okText="确定"
+            cancelText="取消"
+            onConfirm={()=>changeShelfStatus(shelfStatus,_record as any)}
+          >
+            <Switch checked={shelfStatus}  />
+          </Popconfirm>
+        )
+      }
+    },
+    {
+      title: '点击量',
+      dataIndex: 'clickRate',
+      key: 'clickRate',
+      width:80,
+      render: (clickRate: any,_record: any) => {
+        return (
+          <Tooltip placement="top" title={clickRate}>
+            <a
+              onClick={()=>{showDrawer(clickRate,_record)}}
+            >{clickRate}</a>
+          </Tooltip>
+        )
+      }
+    },
+    {
+      title: '操作',
+      key: 'action',
+      render: (_record: any) => {
+        return (
+          <Space size="middle">
+            <a
+              onClick={() => {
+                history.push(`/science-technology-manage/column-manage/detail?id=${_record.id}`);
+              }}
+            >查看</a>
+            <a
+              onClick={() => {
+                history.push(`/science-technology-manage/column-manage/edit?id=${_record.id}`);
+              }}
+            >编辑</a>
+            <Popconfirm
+              title="确定删除？"
+              okText="确定"
+              cancelText="取消"
+              onConfirm={()=>{remove(_record)}}
+            >
+              <a>删除</a>
+            </Popconfirm>
+          </Space>
+        )
+      }
+    }
+  ].filter(p => p);
+  const clickColumns = [
+    {
+      title: '姓名',
+      dataIndex: 'name',
+    },
+    {
+      title: '手机号',
+      dataIndex: 'phone',
+    },
+    {
+      title: '点击时间',
+      dataIndex: 'clickTime',
+    },
+    {
+      title: '浏览端',
+      dataIndex: 'clientSource',
+    },
+  ];
+  // const clickDataSource=[
+  //   {key:1,name:'nihao',phone:'14234553425342',clickTime:1234,clientSource:'app端'},
+  //   {key:2,name:'nihao1',phone:'542352345234',clickTime:54325,clientSource:'app端'},
+  //   {key:3,name:'nihao2',phone:'54325534255',clickTime:53425,clientSource:'web端'},
+  //   {key:4,name:'nihao3',phone:'53425234534',clickTime:53425423,clientSource:'web端'},
+  // ]
+  const rowSelection = {
+    getCheckboxProps: (record: any) => ({
+      name: record.name,
+    }),
+  };
   const useSearchNode = (): React.ReactNode => {
     const [searchForm] = Form.useForm();
     const formLayout = {
@@ -40,17 +262,17 @@ export default () => {
         <Form {...formLayout} form={searchForm}>
           <Row>
             <Col span={8}>
-              <Form.Item name="columnName" label="专栏名称">
+              <Form.Item name="name" label="专栏名称">
                 <Input placeholder="请输入" />
               </Form.Item>
             </Col>
             <Col span={8}>
-              <Form.Item name="state" label="上下架状态">
-                  <Select placeholder="请选择" allowClear>
-                    <Select.Option value={'AUDIT_PASSED'}>上架</Select.Option>
-                    <Select.Option value={'AUDIT_REJECTED'}>下架</Select.Option>
-                  </Select>
-                </Form.Item>
+              <Form.Item name="shelfStatus" label="上下架状态">
+                <Select placeholder="请选择" allowClear>
+                  <Select.Option value={true}>上架</Select.Option>
+                  <Select.Option value={false}>下架</Select.Option>
+                </Select>
+              </Form.Item>
             </Col>
             <Col span={8}>
               <Button
@@ -59,13 +281,7 @@ export default () => {
                 key="search"
                 onClick={() => {
                   const search = searchForm.getFieldsValue();
-                  const { time, ...rest } = search;
-                  console.log(time)
-                  if (time) {
-                    rest.startTime = moment(search.time[0]).format('YYYY-MM-DD');
-                    rest.endTime = moment(search.time[1]).format('YYYY-MM-DD');
-                  }
-                  setSearChContent(rest);
+                  setSearChContent(search);
                 }}
               >
                 查询
@@ -86,80 +302,188 @@ export default () => {
       </div>
     );
   };
-  const columns = [
-    {
-      title: '顺序序号',
-      dataIndex: 'sort',
-      key: 'sort',
-      width:120,
-      render: (_: any, _record: any, index: number) =>
-        pageInfo.pageSize * (pageInfo.pageIndex - 1) + index + 1,
-    },
-    {
-      title: '专栏名字',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: '专栏介绍',
-      dataIndex: 'intro',
-      key: 'intro',
-    },
-    {
-      title: '上下架状态',
-      key: 'started',
-      dataIndex: 'started',
-      width:100,
-      render: (started: boolean) => {
-        return (
-              <Switch  style={{ marginRight: 20 }} checked={started}  />
-        )
-      }
-    },
-    {
-      title: '点击量',
-      dataIndex: 'num',
-      key: 'num',
-      width:80,
-    },
-    {
-      title: '操作',
-      key: 'action',
-      render: () => {
-        return (
-          <Space size="middle">
-            <a
-              onClick={() => {
-                history.push(`/science-technology-manage/column-manage/detail`);
-              }}
-            >查看</a>
-            <a
-              onClick={() => {
-                history.push(`/science-technology-manage/column-manage/edit`);
-              }}
-            >编辑</a>
-            <Popconfirm
-              title="确定删除？"
-              okText="确定"
-              cancelText="取消"
-            >
-              <a>删除</a>
-            </Popconfirm>
-          </Space>
-        )
-      }
-    }
-  ].filter(p => p);
+  //点击量搜索区域
+  const useClickSearchNode = (): React.ReactNode => {
+    const [searchClickForm] = Form.useForm();
+    const formLayout = {
+      labelCol: { span: 6 },
+      wrapperCol: { span: 16 },
+    };
+    return (
+      <div className={sc('container-search')}>
+        <Form {...formLayout} form={searchClickForm}>
+          <Row>
+            <Col span={8}>
+              <Form.Item name="columnName" >
+                <Input placeholder="请输入姓名或手机号" />
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Form.Item name="state" label="浏览端">
+                <Select placeholder="请选择" allowClear>
+                  <Select.Option value={'AUDIT_PASSED'}>上架</Select.Option>
+                  <Select.Option value={'AUDIT_REJECTED'}>下架</Select.Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Button
+                style={{ marginRight: 20, marginLeft: 20 }}
+                type="primary"
+                key="search"
+                onClick={() => {
+                  const search = searchClickForm.getFieldsValue();
+                  setSearChContent(search);
+                }}
+              >
+                查询
+              </Button>
+              <Button
+                type="primary"
+                key="reset"
+                onClick={() => {
+                  searchClickForm.resetFields();
+                  setSearChContent({});
+                }}
+              >
+                重置
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </div>
+    );
+  };
+  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
 
+  const exportList = async (selected: boolean) => {
+    if (selected && !selectedRowKeys.length) {
+      message.warning('请选择数据');
+      return;
+    }
+    try {
+      let data = {};
+      if (selected) {
+        data = { ids: [...selectedRowKeys] };
+      } else {
+        data = { ...searchContent };
+      }
+      const res = await loanClickExport({ ...data });
+      const content = res?.data;
+      const blob = new Blob([content], {
+        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8',
+      });
+      const fileName = '点击详情列表.xlsx';
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.style.display = 'none';
+      link.href = url;
+      link.setAttribute('download', fileName);
+      document.body.appendChild(link);
+      link.click();
+      setTimeout(() => {
+        message.success(`导出成功`);
+      }, 1000);
+    } catch (error) {
+      console.log(error);
+      message.error(`导出失败`);
+    }
+  };
+
+  const menuProps = (
+    <Menu>
+      <Menu.Item icon={<UploadOutlined />} onClick={() => exportList(false)}>
+        导出筛选结果
+      </Menu.Item>
+      <Menu.Item icon={<UploadOutlined />} onClick={() => exportList(true)}>
+        导出选中数据
+      </Menu.Item>
+    </Menu>
+  );
+
+  //点击量的抽屉面板
+  const useClickDrawer = (): React.ReactNode => {
+    return (
+      <Drawer
+        title="点击量"
+        closable={true}
+        width={720}
+        onClose={onClickClose}
+        visible={clickOpen}
+        className={'column-manage-drawer'}
+        bodyStyle={{ paddingBottom: 80 }}
+        footer={
+           (
+            <Space>
+              <Button onClick={onClickClose}>取消</Button>
+              <Button
+                type="primary"
+                onClick={() => {
+                }}
+              >
+                确定
+              </Button>
+            </Space>
+          )
+        }
+        footerStyle={{ display: 'flex', justifyContent: 'end' }}
+      >
+        <div>
+        {useClickSearchNode()}
+          <Dropdown overlay={menuProps}>
+            <Button size="large">
+              <Space>
+                导出
+                <DownOutlined />
+              </Space>
+            </Button>
+          </Dropdown>
+
+        {/*  <Button style={{marginBottom:'20px'}} icon={<UploadOutlined />} onClick={exportList}>*/}
+        {/*  导出*/}
+        {/*</Button>*/}
+        <Table
+          bordered
+          rowKey={'id'}
+          rowSelection={{
+            fixed: true,
+            selectedRowKeys,
+            onChange: onSelectChange,
+          }}
+
+          columns={clickColumns}
+          dataSource={clickDataSource}
+          rowKey={'key'}
+          pagination={
+            clickPageInfo.totalCount === 0
+              ? false
+              : {
+                onChange: getColumnList,
+                total: clickPageInfo.totalCount,
+                current: clickPageInfo.pageIndex,
+                pageSize: clickPageInfo.pageSize,
+                showTotal: (total) =>
+                  `共${total}条记录 第${clickPageInfo.pageIndex}/${Math.ceil(clickPageInfo.totalCount / clickPageInfo.pageSize) || 1}页`,
+              }
+          }
+        />
+        </div>
+      </Drawer>
+    );
+  };
   return(
     <PageContainer className={sc('container')}>
+      <>
       {useSearchNode()}
       <div style={{ backgroundColor: '#fff', padding: 20 }}>
-        <div className={sc('container-header')}>
+        <div className={sc('container-header')} style={{ marginBottom:20 }}>
             <Button
               type="primary"
               key="newAdd"
               onClick={() => {
+                history.push(`/science-technology-manage/column-manage/edit`);
               }}
             >
               新增专栏
@@ -172,19 +496,22 @@ export default () => {
             dataSource={dataSource}
             rowKey={'id'}
             pagination={
-              pageInfo.total === 0
+              pageInfo.totalCount === 0
                 ? false
                 : {
-                  total: pageInfo.total,
+                  onChange:getColumnList,
+                  total: pageInfo.totalCount,
                   current: pageInfo.pageIndex,
                   pageSize: pageInfo.pageSize,
                   showTotal: (total) =>
-                    `共${total}条记录 第${pageInfo.pageIndex}/${Math.ceil(pageInfo.total / pageInfo.pageSize) || 1}页`,
+                    `共${total}条记录 第${pageInfo.pageIndex}/${Math.ceil(pageInfo.totalCount / pageInfo.pageSize) || 1}页`,
                 }
             }
           />
         </div>
       </div>
+      {useClickDrawer()}
+      </>
     </PageContainer>
   )
 }
