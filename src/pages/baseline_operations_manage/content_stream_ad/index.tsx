58a0a2d48c48496c1dc862f50f12e45758270352
flytc <51993313+flyman-ops@users.noreply.github.com>
2023-05-04 14:50:26 +0800

    feat:运营位配置管理

--- src/pages/baseline_operations_manage/content_stream_ad/index.tsx
+++ src/pages/baseline_operations_manage/content_stream_ad/index.tsx
@@ -1,25 +1,14 @@
-import {
-  Button,
-  message,
-  Space,
-  Popconfirm,
-  Tooltip,
-  Select,
-  Row,
-  Col,
-  Form,
-  Input,
-} from 'antd';
-import type { ProColumns, ActionType } from '@ant-design/pro-table';
-import type SolutionTypes from '@/types/solution';
+import {Button, Select, Row, Col, Form, Input, Popconfirm, message, message as antdMessage} from 'antd';
 import { PageContainer } from '@ant-design/pro-layout';
-import ProTable from '@ant-design/pro-table';
-import React, { useEffect, useState, useRef } from 'react';
+import React, {useEffect, useState} from 'react';
 import SelfTable from '@/components/self_table';
 import { history, Access, useAccess } from 'umi';
-import './index.less'
+import './index.less';
 import scopedClasses from '@/utils/scopedClasses';
 import { routeName } from '../../../../config/routes';
+import {updateConversion} from "@/services/achievements-manage";
+import {updateLabel} from "@/services/purchase";
+import {getAllLayout, getGlobalFloatAdDetail, getPartLabels} from "@/services/baseline";
 
 const sc = scopedClasses('content-stream-ad');
 
@@ -38,46 +27,58 @@ const staNumArr = [
   },
   {
     title: '人工智能上架总数',
-    num: 0
+    num: 0,
   },
   {
     title: '智能家电上架总数',
-    num: 0
+    num: 0,
   },
   {
     title: '汽车上架总数',
-    num: 0
+    num: 0,
   },
-]
-
+];
+const handleAdd = () => {
+  // history.push(`${routeName.BASELINE_OPERATIONS_MANAGEMENT_HOME_SCREEN_AD_ADD}`)
+  window.open(`${routeName.BASELINE_OPERATIONS_MANAGEMENT_CONTENT_STREAM_AD_ADD}`);
+};
+const handleDetail = () => {
+  window.open(`${routeName.BASELINE_OPERATIONS_MANAGEMENT_CONTENT_STREAM_AD_DETAIL}`);
+};
+const handleStatisticalDetail = (title: string) => {
+  history.push(
+    `${routeName.BASELINE_OPERATIONS_MANAGEMENT_CONTENT_STREAM_AD_STATISTICAL_DETAIL}?title=${title}`,
+  );
+};
 // 统计卡片
 const StaCard = () => {
   return (
-  <div className={sc('card')}>
-    {staNumArr.map(((item) => {
-      return (
-        <div className="wrap" key={item.title}>
-          <div className="title">{ item.title }</div>
-          <div className="num">{ item.num }</div>
-        </div>
-      )
-    }))}
-  </div>
-  )
-}
-
-
+    <div className={sc('card')}>
+      {staNumArr.map((item) => {
+        return (
+          <div
+            className="wrap"
+            key={item.title}
+            onClick={() => handleStatisticalDetail(item.title)}
+          >
+            <div className="title">{item.title} ></div>
+            <div className="num">{item.num}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 const formLayout = {
   labelCol: { span: 4 },
   wrapperCol: { span: 16 },
-}
-
-
+};
 
 // 搜索模块
-const useSearchNode = (): React.ReactNode => {
+  const useSearchNode = (): React.ReactNode => {
   const [searchForm] = Form.useForm();
+  const [layType, setLayType] = useState<any>([]);
   const [searchContent, setSearChContent] = useState<any>({});
   return (
     <div className={sc('container-search')}>
@@ -85,15 +86,16 @@ const useSearchNode = (): React.ReactNode => {
         <Row>
           <Col span={6}>
             <Form.Item name="topic" label="内容标题">
-              <Input placeholder="请输入" allowClear  autoComplete="off"/>
+              <Input placeholder="请输入" allowClear autoComplete="off" />
             </Form.Item>
           </Col>
           <Col span={6}>
             <Form.Item name="publicUserName" label="版面">
-              <Select placeholder="请选择" allowClear >
-                <Select.Option value={0}>下架</Select.Option>
-                <Select.Option value={1}>上架</Select.Option>
-              </Select>
+              <Select options={layType} mode={'multiple'} placeholder="请选择" />
+              {/*<Select placeholder="请选择" allowClear>*/}
+              {/*  <Select.Option value={0}>下架</Select.Option>*/}
+              {/*  <Select.Option value={1}>上架</Select.Option>*/}
+              {/*</Select>*/}
             </Form.Item>
           </Col>
           <Col span={6}>
@@ -101,13 +103,13 @@ const useSearchNode = (): React.ReactNode => {
               <Select placeholder="请选择" allowClear>
                 <Select.Option value={0}>下架</Select.Option>
                 <Select.Option value={1}>上架</Select.Option>
-                <Select.Option value={2}>暂存</Select.Option>
+                {/*<Select.Option value={2}>暂存</Select.Option>*/}
               </Select>
             </Form.Item>
           </Col>
           <Col span={4}>
             <Button
-              style={{ marginRight:'20px' }}
+              style={{ marginRight: '20px' }}
               type="primary"
               key="search"
               onClick={() => {
@@ -130,27 +132,12 @@ const useSearchNode = (): React.ReactNode => {
         </Row>
       </Form>
     </div>
-  )
-}
-
-
-
+  );
+};
 
 export default () => {
-
-  const handleAdd = () => {
-    // history.push(`${routeName.BASELINE_OPERATIONS_MANAGEMENT_HOME_SCREEN_AD_ADD}`)
-    window.open(`${routeName.BASELINE_OPERATIONS_MANAGEMENT_CONTENT_STREAM_AD_ADD}`)
-  }
-  const handleDetail = () => {
-    window.open(`${routeName.BASELINE_OPERATIONS_MANAGEMENT_CONTENT_STREAM_AD_DETAIL}`)
-  }
-  const handleStatisticalDetail = () => {
-    history.push(`${routeName.BASELINE_OPERATIONS_MANAGEMENT_CONTENT_STREAM_AD_STATISTICAL_DETAIL}`)
-  }
-
   // 拿到当前角色的access权限兑现
-  const access = useAccess()
+  const access = useAccess();
   const [dataSource, setDataSource] = useState<any>([]);
   // const [searchContent, setSearChContent] = useState<any>({});
   const [visible, setVisible] = useState<any>(false);
@@ -162,8 +149,44 @@ export default () => {
     totalCount: 0,
     pageTotal: 0,
   });
-
-
+  // 删除
+  const remove = async (id: string) => {
+    try {
+      const removeRes = await updateLabel({id, state: 1, labelType: 0});
+      if (removeRes.code === 0) {
+        message.success(`删除成功`);
+      } else {
+        message.error(`删除失败，原因:${removeRes.message}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const editState = async (id: string) => {
+    try {
+      const updateStateResult = await updateConversion(id);
+      if (updateStateResult.code === 0) {
+        message.success(`操作成功`);
+      } else {
+        message.error(`操作失败，请重试`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAllLayout().then((res) => {
+      if (res.code === 0 && res.result) {
+        const labelArr = res.result.map((item: any) => {
+          return {
+            value: item.id,
+            label: item.typeName,
+          };
+        });
+        setLayType(labelArr);
+      }
+    });
+  }, []);
   const columns = [
     {
       title: '序号',
@@ -177,8 +200,8 @@ export default () => {
       dataIndex: 'title',
       width: 150,
       render: (title: string) => {
-        return (title || '--')
-      }
+        return title || '--';
+      },
     },
     {
       title: '图片',
@@ -186,8 +209,8 @@ export default () => {
       isEllipsis: true,
       width: 250,
       render: (newUrl: string) => {
-        return (newUrl || '--')
-      }
+        return newUrl || '--';
+      },
     },
     {
       title: '版面',
@@ -195,47 +218,50 @@ export default () => {
       isEllipsis: true,
       width: 250,
       render: () => {
-        return <div>123</div>
-      }
+        return <div>123</div>;
+      },
     },
     {
       title: '位置',
       dataIndex: 'crawered',
       width: 150,
       render: () => {
-        return <div>123</div>
-      }
+        return <div>123</div>;
+      },
     },
     {
       title: '作用范围',
       width: 150,
       dataIndex: 'sended',
       render: (sended: number) => {
-        return <span>{sended === 1 ? '是' : '否'}</span>
-      }
+        return <span>{sended === 1 ? '是' : '否'}</span>;
+      },
     },
     {
       title: '浏览次数',
       width: 200,
       dataIndex: 'createTime',
+      sorter: (a: any, b: any) => a.createTime - b.createTime,
       render: (createTime: string) => {
-        return <span>{createTime || '--'}</span>
-      }
+        return <span>{createTime || '--'}</span>;
+      },
     },
     {
       title: '被关闭次数',
       dataIndex: 'createByName',
       width: 200,
+      sorter: (a: any, b: any) => a.createByName - b.createByName,
       render: (createByName: string) => {
-        return (createByName || '--')
+        return createByName || '--';
       },
     },
     {
       title: '曝光量',
       dataIndex: 'createByName',
       width: 200,
+      sorter: (a: any, b: any) => a.createByName - b.createByName,
       render: (createByName: string) => {
-        return (createByName || '--')
+        return createByName || '--';
       },
     },
     {
@@ -243,15 +269,15 @@ export default () => {
       dataIndex: 'updateTime',
       width: 200,
       render: (updateTime: string) => {
-        return <span>{updateTime || '--'}</span>
-      }
+        return <span>{updateTime || '--'}</span>;
+      },
     },
     {
       title: '状态',
       dataIndex: 'createByName',
       width: 200,
       render: (createByName: string) => {
-        return (createByName || '--')
+        return createByName || '--';
       },
     },
     {
@@ -259,12 +285,36 @@ export default () => {
       width: 150,
       fixed: 'right',
       render: (_: any, record: any) => {
-        if (record.crawered === 0){
-          return <span>--</span>
+        if (record.crawered === 0) {
+          return <span>--</span>;
         }
         return (
-          <div style={{whiteSpace: 'break-spaces'}}>
-            <Access accessible={access['PD_BLM_SSRDGL']}>
+          <div style={{ whiteSpace: 'break-spaces' }}>
+            <Access accessible={access.PD_BLM_SSRDGL}>
+              <Button
+                size="small"
+                type="link"
+                onClick={() => {
+                  // handleDelete(record)
+                }}
+              >
+                详情
+              </Button>
+              <Button
+                size="small"
+                type="link"
+                onClick={() => {
+                  // handleDelete(record)
+                }}
+              >
+                编辑
+              </Button>
+              <Popconfirm
+                title="删除该内容流广告后，系统将不再推荐该广告，确定删除？"
+                okText="删除"
+                cancelText="取消"
+                onConfirm={() => remove(record.id as string)}
+              >
               <Button
                 size="small"
                 type="link"
@@ -274,28 +324,47 @@ export default () => {
               >
                 删除
               </Button>
+              </Popconfirm>
+              {record.state === 'ON_SHELF' && (
+                <Popconfirm
+                  title="确定将内容下架？"
+                  okText="下架"
+                  cancelText="取消"
+                  onConfirm={() => editState(record.id as any, 0)}
+                >
+                  <Button type="link">下架</Button>
+                </Popconfirm>
+              )}
+              {record.state === 'OFF_SHELF' && (
+                <Popconfirm
+                  title="确定将内容上架？"
+                  okText="上架"
+                  cancelText="取消"
+                  onConfirm={() => editState(record.id as any, 1)}
+                >
+                  <Button type="link">上架</Button>
+                </Popconfirm>
+              )}
+
             </Access>
           </div>
-        )
+        );
       },
     },
   ];
-
-
   return (
     <PageContainer className={sc('container')}>
       <StaCard />
       {useSearchNode()}
       <div className={sc('container-table-body')}>
-        <Access accessible={access['PA_BLM_SSRDGL']}>
+        <Access accessible={access.PA_BLM_SSRDGL}>
           <Button
             type="primary"
-            style={{marginBottom: '10px'}}
+            style={{ marginBottom: '10px' }}
             onClick={() => {
+              window.open(`${routeName.BASELINE_OPERATIONS_MANAGEMENT_CONTENT_STREAM_AD_ADD}`);
               // modalForm.resetFields()
               // setVisible(true)
-              window.open(routeName.BASELINE_OPERATIONS_MANAGEMENT_CONTENT_STREAM_AD_STATISTICAL_DETAIL)
-
             }}
           >
             新增
@@ -312,16 +381,16 @@ export default () => {
             pageInfo.totalCount === 0
               ? false
               : {
-                onChange: getPage,
-                total: pageInfo.totalCount,
-                current: pageInfo.pageIndex,
-                pageSize: pageInfo.pageSize,
-                showTotal: (total: number) =>
-                  `共${total}条记录 第${pageInfo.pageIndex}/${pageInfo.pageTotal || 1}页`,
-              }
+                  onChange: getPage,
+                  total: pageInfo.totalCount,
+                  current: pageInfo.pageIndex,
+                  pageSize: pageInfo.pageSize,
+                  showTotal: (total: number) =>
+                    `共${total}条记录 第${pageInfo.pageIndex}/${pageInfo.pageTotal || 1}页`,
+                }
           }
         />
       </div>
     </PageContainer>
-  )
-}
+  );
+};
