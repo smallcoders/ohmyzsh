5a436454a8e0a79d100c9cd2b00d2cb48b3eef9e
lltang11 <lltang11@iflytek.com>
2023-05-25 15:31:20 +0800

    feat: 选择项

--- src/pages/business_channel/opportunity/components/auditModal.tsx
+++ src/pages/business_channel/opportunity/components/auditModal.tsx
@@ -124,9 +124,9 @@ const UploadModal = forwardRef((props: any, ref: any) => {
             if (result.code === 0) {
               setChannelList(result.result?.map((item: any) => {
                 return {
-                  label: historyChannel.indexOf(item.id) !== -1 ? `${item.channelName}    历史渠道商` : item.channelName,
+                  label: res.result.indexOf(item.id) !== -1 ? `${item.channelName}    历史渠道商` : item.channelName,
                   value: item.id,
-                  disabled: historyChannel.indexOf(item.id) !== -1,
+                  disabled: res.result.indexOf(item.id) !== -1,
                   areaCode: item.areaCode,
                   cityCode: item.cityCode
                 }
@@ -260,7 +260,7 @@ const UploadModal = forwardRef((props: any, ref: any) => {
       </div>
     )
   }
-  console.log(modalType, 'tmpList')
+  console.log(channelList, 'tmpList')
   return (
     <Modal
       title={
