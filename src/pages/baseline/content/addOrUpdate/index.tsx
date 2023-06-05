0ef9dd1f9d570cbe31ef1726172853e49e255d28
flytc <51993313+flyman-ops@users.noreply.github.com>
2023-06-05 09:26:50 +0800

    feat:运营广告位

--- src/pages/baseline/content/addOrUpdate/index.tsx
+++ src/pages/baseline/content/addOrUpdate/index.tsx
@@ -1,13 +1,32 @@
-import { message, Image, Button, Form, Select, Input, Breadcrumb, Modal, DatePicker } from 'antd';
+import {
+  message,
+  Image,
+  Button,
+  Form,
+  Select,
+  Input,
+  Breadcrumb,
+  Modal,
+  DatePicker,
+  Card,
+} from 'antd';
 import { Access, history, Link, Prompt, useAccess } from 'umi';
 import { useState, useEffect } from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 import scopedClasses from '@/utils/scopedClasses';
 import './index.less';
 import FormEdit from '@/components/FormEdit';
-import { addArticle, auditArticle, editArticle, getArticleDetail, getArticleTags, getArticleType } from '@/services/baseline';
+import {
+  addArticle,
+  auditArticle,
+  editArticle,
+  getArticleDetail,
+  getArticleTags,
+  getArticleType,
+} from '@/services/baseline';
 import { routeName } from '../../../../../config/routes';
 import moment from 'moment';
+import { PlusOutlined } from '@ant-design/icons';
 
 const sc = scopedClasses('science-technology-manage-creative-detail');
 
@@ -16,13 +35,14 @@ export default () => {
   const [tags, setTags] = useState<any>([]);
   const [types, setTypes] = useState<any>([]);
   const id = history.location.query?.id as string;
-  const access = useAccess()
+  const { TextArea } = Input;
+  const access = useAccess();
   const prepare = async () => {
     if (id) {
       try {
         const res = await getArticleDetail(id);
         if (res.code === 0) {
-          const detail = res?.result
+          const detail = res?.result;
 
           form.setFieldsValue({
             title: detail?.title,
@@ -30,11 +50,11 @@ export default () => {
             sourceUrl: detail?.sourceUrl,
             author: detail?.author,
             keywords: detail?.keywords,
-            types: detail?.types?.length > 0 ? detail?.types?.map(p => p.id) : undefined,
-            labels: detail?.labels?.length > 0 ? detail?.labels?.map(p => p.id) : undefined,
+            types: detail?.types?.length > 0 ? detail?.types?.map((p) => p.id) : undefined,
+            labels: detail?.labels?.length > 0 ? detail?.labels?.map((p) => p.id) : undefined,
             content: detail?.content,
-            publishTime: detail?.publishTime ? moment(detail?.publishTime) : undefined
-          })
+            publishTime: detail?.publishTime ? moment(detail?.publishTime) : undefined,
+          });
         } else {
           throw new Error(res.message);
         }
@@ -44,7 +64,7 @@ export default () => {
         setLoading(false);
       }
     }
-    getDic()
+    getDic();
   };
 
   const getDic = async () => {
@@ -55,7 +75,7 @@ export default () => {
     } catch (error) {
       message.error('获取数据失败');
     }
-  }
+  };
   const listener = (e: any) => {
     e.preventDefault();
     e.returnValue = '离开当前页后，所编辑的数据将不可恢复';
@@ -77,72 +97,73 @@ export default () => {
 
   const onSubmit = async (status: number) => {
     try {
-      const data = form.getFieldsValue()
+      const data = form.getFieldsValue();
       if (data.publishTime) {
-        data.publishTime = moment(data.publishTime).valueOf()
+        data.publishTime = moment(data.publishTime).valueOf();
       }
 
       const cb = async () => {
-        setLoading(true)
-        const res = await (id ? editArticle({ id, ...data, status }) : addArticle({ ...data, status }))
+        setLoading(true);
+        const res = await (id
+          ? editArticle({ id, ...data, status })
+          : addArticle({ ...data, status }));
         if (res?.code == 0) {
-          message.success('操作成功')
+          message.success('操作成功');
           setIsClosejumpTooltip(false);
-          status == 1 && history.push(routeName.BASELINE_CONTENT_MANAGE)
+          status == 1 && history.push(routeName.BASELINE_CONTENT_MANAGE);
         } else {
-          message.error(res?.message || '操作失败')
+          message.error(res?.message || '操作失败');
         }
-        setLoading(false)
-      }
+        setLoading(false);
+      };
 
       if (status == 1) {
-        await form.validateFields()
+        await form.validateFields();
         Modal.confirm({
           title: '提示',
           content: '确定将内容上架？',
           onOk: async () => {
-            beforeUp(data.content, cb)
+            beforeUp(data.content, cb);
           },
           onCancel: () => {
-            return
+            return;
           },
-          okText: '上架'
-        })
+          okText: '上架',
+        });
       } else {
-        cb()
+        cb();
       }
-
     } catch (error) {
-      setLoading(false)
-      console.log(' error ', error)
+      setLoading(false);
+      console.log(' error ', error);
     }
-  }
+  };
 
   const beforeUp = async (content: string, cb: () => void) => {
     try {
-      const res = await auditArticle(content)
+      const res = await auditArticle(content);
       if (res?.result) {
         Modal.confirm({
           title: '风险提示',
           content: res?.result,
           onOk: async () => {
-            cb()
+            cb();
           },
-          onCancel: () => {
-          },
+          onCancel: () => {},
           okText: '继续上架',
-          cancelText: '返回更改'
-        })
+          cancelText: '返回更改',
+        });
       } else {
-        cb()
+        cb();
       }
     } catch (error) {
-      console.log(' error ', error)
+      console.log(' error ', error);
     }
-  }
+  };
   const [isClosejumpTooltip, setIsClosejumpTooltip] = useState<boolean>(true);
   return (
-    <PageContainer loading={loading}
+    <PageContainer
+      loading={loading}
       header={{
         title: id ? `内容编辑` : '添加内容',
         breadcrumb: (
@@ -153,15 +174,20 @@ export default () => {
             <Breadcrumb.Item>
               <Link to="/baseline">内容管理</Link>
             </Breadcrumb.Item>
-            <Breadcrumb.Item>
-              {id ? `内容编辑` : '添加内容'}
-            </Breadcrumb.Item>
+            <Breadcrumb.Item>{id ? `内容编辑` : '添加内容'}</Breadcrumb.Item>
           </Breadcrumb>
         ),
       }}
       footer={[
-        <Access accessible={access['PA_BLM_NRGL']}> <Button type="primary" onClick={() => onSubmit(1)}>立即上架</Button></Access>,
-        <Access accessible={access['PA_BLM_NRGL']}><Button onClick={() => onSubmit(2)}>暂存</Button></Access>,
+        <Access accessible={access.PA_BLM_NRGL}>
+          {' '}
+          <Button type="primary" onClick={() => onSubmit(1)}>
+            立即上架
+          </Button>
+        </Access>,
+        <Access accessible={access.PA_BLM_NRGL}>
+          <Button onClick={() => onSubmit(2)}>暂存</Button>
+        </Access>,
         <Button onClick={() => history.push(routeName.BASELINE_CONTENT_MANAGE)}>返回</Button>,
       ]}
     >
@@ -170,43 +196,26 @@ export default () => {
         message={'离开此页面，将不会保存当前编辑的内容，确认离开吗？'}
       />
       <div className={sc('container')}>
-        <Form
-          form={form}
-          name="basic"
-          {...formLayout}
-          validateTrigger={['onBlur']}
-        >
-          <Form.Item
-            label="标题"
-            name="title"
-            rules={[{ required: true, message: '请输入' }]}
-          >
+        <Form form={form} name="basic" {...formLayout} validateTrigger={['onBlur']}>
+          <Form.Item label="标题" name="title" rules={[{ required: true, message: '请输入' }]}>
             <Input.TextArea
-                    placeholder="请输入"
-                    autoSize={{ minRows: 3, maxRows: 5 }}
-                    maxLength={200}
-                  />
+              placeholder="请输入"
+              autoSize={{ minRows: 3, maxRows: 5 }}
+              maxLength={200}
+            />
           </Form.Item>
-          <Form.Item
-            label="来源"
-            name="source"
-            rules={[{ required: true, message: '请输入' }]}
-          >
+          <Form.Item label="来源" name="source" rules={[{ required: true, message: '请输入' }]}>
             <Input maxLength={35} placeholder="请输入" allowClear />
           </Form.Item>
-
-          <Form.Item
-            label="作者"
-            name="author"
-
-          >
+          <Form.Item label="作者" name="author">
             <Input maxLength={35} placeholder="请输入" allowClear />
           </Form.Item>
-          <Form.Item
-            label="关键词"
-            name="keywords"
-          >
-            <Input maxLength={35} placeholder="请输入,多个关键词之间请使用英文逗号分隔" allowClear />
+          <Form.Item label="关键词" name="keywords">
+            <Input
+              maxLength={35}
+              placeholder="请输入,多个关键词之间请使用英文逗号分隔"
+              allowClear
+            />
           </Form.Item>
           <Form.Item
             label="内容发布时间"
@@ -215,11 +224,7 @@ export default () => {
           >
             <DatePicker showTime style={{ width: '100%' }} />
           </Form.Item>
-          <Form.Item
-            label="内容类型"
-            name="types"
-            rules={[{ required: true, message: '请输入' }]}
-          >
+          <Form.Item label="内容类型" name="types" rules={[{ required: true, message: '请输入' }]}>
             <Select mode="multiple" placeholder="请选择" allowClear>
               {types?.map((item: any) => (
                 <Select.Option key={item?.id} value={Number(item?.id)}>
@@ -228,12 +233,8 @@ export default () => {
               ))}
             </Select>
           </Form.Item>
-          <Form.Item
-            label="标签"
-            name="labels"
-            rules={[{ required: true, message: '请输入' }]}
-          >
-            <Select mode="multiple" placeholder="请选择" allowClear >
+          <Form.Item label="标签" name="labels" rules={[{ required: true, message: '请输入' }]}>
+            <Select mode="multiple" placeholder="请选择" allowClear>
               {tags?.map((item: any) => (
                 <Select.Option key={item?.id} value={Number(item?.id)}>
                   {item?.labelName}
@@ -244,12 +245,83 @@ export default () => {
           <Form.Item name="content" label="内容" rules={[{ required: true, message: '请输入' }]}>
             <FormEdit width={624} />
           </Form.Item>
-          <Form.Item
-            label="文章原址"
-            name="sourceUrl"
-          >
+          <Form.Item label="文章原址" name="sourceUrl">
             <Input maxLength={35} placeholder="请输入" allowClear />
           </Form.Item>
+          <div style={{ marginLeft: '100px' }}>
+            <span style={{ fontSize: '20px', fontWeight: 500 }}>链接</span>
+            <Form.List name="links">
+              {(fields, { add, remove }) => (
+                <>
+                  <Form.Item>
+                    <Button
+                      style={{ margin: '10px 0' }}
+                      type="primary"
+                      disabled={fields.length >= 5}
+                      key="addStyle1"
+                      onClick={() => add()}
+                    >
+                      <PlusOutlined /> 新增
+                    </Button>
+                  </Form.Item>
+                  {fields.map((field, index) => (
+                    <Card
+                      key={field.key}
+                      title={`链接${index + 1}`}
+                      extra={
+                        <a
+                          key="del"
+                          onClick={() => {
+                            remove(field.name);
+                          }}
+                        >
+                          删除
+                        </a>
+                      }
+                      style={{ width: 600, marginBottom: '10px' }}
+                    >
+                      <Form.Item
+                        {...field}
+                        name={[field.name, 'title']}
+                        label="链接标题"
+                        rules={[
+                          {
+                            required: true,
+                            message: `必填`,
+                          },
+                        ]}
+                      >
+                        <TextArea
+                          autoSize={{ minRows: 1, maxRows: 4 }}
+                          placeholder="请输入"
+                          maxLength={10}
+                        />
+                      </Form.Item>
+                      <Form.Item name={[field.name, 'introduction']} label="链接简介">
+                        <TextArea autoSize={{ minRows: 2, maxRows: 10 }} placeholder="请输入" />
+                      </Form.Item>
+                      <Form.Item
+                        name={[field.name, 'address']}
+                        label="链接地址"
+                        rules={[
+                          {
+                            required: true,
+                            message: `必填`,
+                          },
+                        ]}
+                      >
+                        <TextArea
+                          autoSize={{ minRows: 2, maxRows: 10 }}
+                          placeholder="请输入"
+                          maxLength={50}
+                        />
+                      </Form.Item>
+                    </Card>
+                  ))}
+                </>
+              )}
+            </Form.List>
+          </div>
         </Form>
       </div>
     </PageContainer>
--- src/pages/baseline/content/detail/index.tsx
+++ src/pages/baseline/content/detail/index.tsx
@@ -1,13 +1,13 @@
-import { message, Button, Avatar } from 'antd';
+import { message, Button, Avatar, Tooltip, Space, Modal } from 'antd';
 import { Access, history, useAccess } from 'umi';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 import scopedClasses from '@/utils/scopedClasses';
 import './index.less';
 import SelfTable from '@/components/self_table';
 import { UserOutlined } from '@ant-design/icons';
 import { getArticleDetail, getArticleStatisticPage } from '@/services/baseline';
-import Common from '@/types/common';
+import type Common from '@/types/common';
 import moment from 'moment';
 import { routeName } from '../../../../../config/routes';
 
@@ -18,11 +18,21 @@ const hostMap = {
   'http://10.103.142.222': 'https://greenenv.lingyangplat.com',
   'http://manage.lingyangplat.com': 'https://www.lingyangplat.com',
   'https://manage.lingyangplat.com': 'https://www.lingyangplat.com',
-  'http://localhost:8000': 'http://172.30.33.222'
-}
+  'http://localhost:8000': 'http://172.30.33.222',
+};
 
 const sc = scopedClasses('science-technology-manage-creative-detail');
-const operaObj = { ADD: '新增', MODIFY: '修改', DOWN: '下架', UP: '上架', DELETE: '删除', TOPPING: '置顶', CANCEL_TOPPING: '取消置顶', AUDIT: '自动审核', STAGING: '暂存' }
+const operaObj = {
+  ADD: '新增',
+  MODIFY: '修改',
+  DOWN: '下架',
+  UP: '上架',
+  DELETE: '删除',
+  TOPPING: '置顶',
+  CANCEL_TOPPING: '取消置顶',
+  AUDIT: '自动审核',
+  STAGING: '暂存',
+};
 export default () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [detail, setDetail] = useState<any>({});
@@ -46,11 +56,9 @@ export default () => {
 
   useEffect(() => {
     prepare();
-    getPage()
+    getPage();
   }, []);
-  const [activeKey, setActiveKey] = useState<any>(
-    '1'
-  );
+  const [activeKey, setActiveKey] = useState<any>('1');
 
   const [pageInfo, setPageInfo] = useState<Common.ResultPage>({
     pageIndex: 1,
@@ -67,7 +75,7 @@ export default () => {
         pageIndex,
         pageSize,
         targetType: 'ARTICLE',
-        articleId: id
+        articleId: id,
       });
       if (code === 0) {
         setPageInfo({ totalCount, pageTotal, pageIndex, pageSize });
@@ -112,15 +120,40 @@ export default () => {
     {
       title: '用户标签',
       dataIndex: 'labels',
-      render: (_: any[]) => _?.length > 0 ? _?.map(p => p.labelName).join(',') : '--',
+      render: (_: any[]) => (_?.length > 0 ? _?.map((p) => p.labelName).join(',') : '--'),
       isEllipsis: true,
       width: 150,
     },
   ];
   const [dataSource, setDataSource] = useState<any[]>([]);
-  const access = useAccess()
+  const access = useAccess();
+  const columns1 = [
+    {
+      title: '链接标题',
+      dataIndex: 'title',
+      render: (_: any[], record: any) => (
+        <a
+          type="primary"
+          onClick={() => {
+            window.open(record.address);
+          }}
+        >
+          {record.title}
+        </a>
+      ),
+      width: 300,
+    },
+    {
+      title: '链接简介',
+      dataIndex: 'introduction',
+      isEllipsis: true,
+      render: (_: any[]) => _ || '--',
+      width: 300,
+    },
+  ].filter((p) => p);
   return (
-    <PageContainer loading={loading}
+    <PageContainer
+      loading={loading}
       tabList={[
         {
           tab: '基础信息',
@@ -139,7 +172,12 @@ export default () => {
       onTabChange={(key: string) => setActiveKey(key)}
       footer={[
         <Button onClick={() => history.push(routeName.BASELINE_CONTENT_MANAGE)}>返回</Button>,
-        <Access accessible={access['P_BLM_NRGL'] && (detail?.status == 2 || ((!detail?.auditCommon) && detail?.status == 0))}>
+        <Access
+          accessible={
+            access.P_BLM_NRGL &&
+            (detail?.status == 2 || (!detail?.auditCommon && detail?.status == 0))
+          }
+        >
           <Button
             type="default"
             onClick={() => {
@@ -148,12 +186,12 @@ export default () => {
           >
             编辑
           </Button>
-        </Access>
+        </Access>,
       ]}
     >
-      <div className='content'>
-        {
-          activeKey === '1' && <>
+      <div className="content">
+        {activeKey === '1' && (
+          <>
             <div className={sc('container')}>
               <div className={sc('container-desc')}>
                 <span>内容标题：</span>
@@ -169,11 +207,19 @@ export default () => {
               </div>
               <div className={sc('container-desc')}>
                 <span>内容类型：</span>
-                <span>{detail?.types?.length > 0 ? detail?.types?.map(p => p.typeName)?.join('，') : '--'}</span>
+                <span>
+                  {detail?.types?.length > 0
+                    ? detail?.types?.map((p) => p.typeName)?.join('，')
+                    : '--'}
+                </span>
               </div>
               <div className={sc('container-desc')}>
                 <span>标签：</span>
-                <span>{detail?.labels?.length > 0 ? detail?.labels?.map(p => p.labelName)?.join('，') : '--'}</span>
+                <span>
+                  {detail?.labels?.length > 0
+                    ? detail?.labels?.map((p) => p.labelName)?.join('，')
+                    : '--'}
+                </span>
               </div>
               <div className={sc('container-desc')}>
                 <span>来源：</span>
@@ -181,7 +227,11 @@ export default () => {
               </div>
               <div className={sc('container-desc')}>
                 <span>发布时间：</span>
-                <span>{detail?.publishTime ? moment(detail?.publishTime).format('YYYY-MM-DD HH:mm:ss') : '--'}</span>
+                <span>
+                  {detail?.publishTime
+                    ? moment(detail?.publishTime).format('YYYY-MM-DD HH:mm:ss')
+                    : '--'}
+                </span>
               </div>
               <div className={sc('container-desc')}>
                 <span>内容详情：</span>
@@ -193,32 +243,59 @@ export default () => {
               </div>
               <div className={sc('container-desc')}>
                 <span>内容原址：</span>
-                <span>{detail?.sourceUrl ? <a href={detail?.sourceUrl} target="_blank">{detail?.sourceUrl}</a> : '--'}</span>
+                <span>
+                  {detail?.sourceUrl ? (
+                    <a href={detail?.sourceUrl} target="_blank" rel="noreferrer">
+                      {detail?.sourceUrl}
+                    </a>
+                  ) : (
+                    '--'
+                  )}
+                </span>
               </div>
-              {
-                detail.status === 1 && detail.id && <div className={sc('container-desc')}>
+              {detail.status === 1 && detail.id && (
+                <div className={sc('container-desc')}>
                   <span>平台内地址：</span>
                   <span>
-                    <a href={`${hostMap[location.origin] || 'http://172.30.33.222'}/antelope-baseline/industry-moments/#/detail?id=${detail.id}`} target="_blank" rel="noreferrer">
-                      {
-                        `${hostMap[location.origin] || 'http://172.30.33.222'}/antelope-baseline/industry-moments/#/detail?id=${detail.id}`
-                      }
+                    <a
+                      href={`${
+                        hostMap[location.origin] || 'http://172.30.33.222'
+                      }/antelope-baseline/industry-moments/#/detail?id=${detail.id}`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {`${
+                        hostMap[location.origin] || 'http://172.30.33.222'
+                      }/antelope-baseline/industry-moments/#/detail?id=${detail.id}`}
                     </a>
                   </span>
                 </div>
-              }
+              )}
+              {detail.links && detail.links.length > 0 && (
+                <SelfTable
+                  bordered
+                  rowKey={'id'}
+                  columns={columns1}
+                  dataSource={detail.links}
+                  pagination={null}
+                />
+              )}
             </div>
           </>
-        }
-        {
-          activeKey === '2' && <>
-            <div style={{ display: 'flex', gap: 50, fontWeight: 'bold', padding: 10, alignItems: 'center' }}>
-              <span style={{ fontSize: '20px' }}>
-                详情浏览总次数
-              </span>
-              <span style={{ fontSize: '16px' }}>
-                {total}
-              </span>
+        )}
+        {activeKey === '2' && (
+          <>
+            <div
+              style={{
+                display: 'flex',
+                gap: 50,
+                fontWeight: 'bold',
+                padding: 10,
+                alignItems: 'center',
+              }}
+            >
+              <span style={{ fontSize: '20px' }}>详情浏览总次数</span>
+              <span style={{ fontSize: '16px' }}>{total}</span>
             </div>
             <div style={{ padding: 10 }}>
               <span style={{ fontWeight: 'bold', fontSize: '20px' }}>用户浏览详情</span>
@@ -232,38 +309,45 @@ export default () => {
                   pageInfo.totalCount === 0
                     ? false
                     : {
-                      onChange: getPage,
-                      total: pageInfo.totalCount,
-                      current: pageInfo.pageIndex,
-                      pageSize: pageInfo.pageSize,
-                      showTotal: (total: number) =>
-                        `共${total}条记录 第${pageInfo.pageIndex}/${pageInfo.pageTotal || 1}页`,
-                    }
+                        onChange: getPage,
+                        total: pageInfo.totalCount,
+                        current: pageInfo.pageIndex,
+                        pageSize: pageInfo.pageSize,
+                        showTotal: (total: number) =>
+                          `共${total}条记录 第${pageInfo.pageIndex}/${pageInfo.pageTotal || 1}页`,
+                      }
                 }
               />
             </div>
           </>
-        }
-        {
-          activeKey === '3' && <>
-            {detail?.operationRecords?.map(p => {
-              return <div style={{ display: 'flex', gap: 50, marginBottom: 20, alignItems: 'center' }}>
-                <div>
-                  <Avatar icon={<UserOutlined />} />
-                  <span style={{ marginLeft: 10 }}>{p?.userName}</span>
-                </div>
+        )}
+        {activeKey === '3' && (
+          <>
+            {detail?.operationRecords?.map((p) => {
+              return (
+                <div style={{ display: 'flex', gap: 50, marginBottom: 20, alignItems: 'center' }}>
+                  <div>
+                    <Avatar icon={<UserOutlined />} />
+                    <span style={{ marginLeft: 10 }}>{p?.userName}</span>
+                  </div>
 
-                <div style={{ display: 'grid' }}>
-                  <span style={{ color: p?.autoAuditResult ? 'red' : 'initial' }}>{operaObj[p?.operation] || '--'}</span>
-                  {p?.autoAuditResult && <span style={{ color: 'red' }}>{p?.autoAuditResult}</span>}
-                </div>
+                  <div style={{ display: 'grid' }}>
+                    <span style={{ color: p?.autoAuditResult ? 'red' : 'initial' }}>
+                      {operaObj[p?.operation] || '--'}
+                    </span>
+                    {p?.autoAuditResult && (
+                      <span style={{ color: 'red' }}>{p?.autoAuditResult}</span>
+                    )}
+                  </div>
 
-                <span>{p?.createTime ? moment(p?.createTime).format('YYYY-MM-DD HH:mm:ss') : '--'}</span>
-              </div>
-            })
-            }
+                  <span>
+                    {p?.createTime ? moment(p?.createTime).format('YYYY-MM-DD HH:mm:ss') : '--'}
+                  </span>
+                </div>
+              );
+            })}
           </>
-        }
+        )}
       </div>
     </PageContainer>
   );
