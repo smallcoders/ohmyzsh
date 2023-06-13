db743c4db25fea5e59c0ebb80c90d3cb112d14d7
李洋 <15903019006@163.com>
2023-06-13 13:43:26 +0800

    feat: 调整运营平台战役目标链接

--- src/pages/data_center/target_monitoring/index.tsx
+++ src/pages/data_center/target_monitoring/index.tsx
@@ -8,7 +8,7 @@ const sc = scopedClasses('target-monitoring');
 export default () => {
   return (
     <div className={sc()}>
-      <iframe src='http://172.30.93.68:8080/shareDashboard/e5338021d1934751a2244a9945f71f88?type=NONE' />
+      <iframe src='http://172.30.93.68:8080/shareDashboard/2f4b8aab53ed49e48c0239e31297fdad?type=NONE' />
     </div>
   );
 };
